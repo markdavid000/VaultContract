ba7d4530890fbea88114b87271bec6c5ceb5abe6
markdavid000 <davidojukwu59@gmail.com>
2024-02-24 11:55:34 +0100

    first function down

--- contracts/GrantVault.sol
+++ contracts/GrantVault.sol
@@ -18,5 +18,21 @@ contract GrantVault {
     event GrantCreated(address indexed donor, address indexed beneficiary, uint256 grantAmount, uint256 releaseTime);
     event GrantClaimed(address indexed beneficiary, uint256 grantAmount);
 
-    
+    function offerGrant(address _beneficiary, uint256 _releaseTime) external payable {
+        require(_beneficiary != address(0), "Beneficiary's address cannot be zero");
+        require(msg.value > 0, "you can't grant zero value");
+        require(_releaseTime > block.timestamp, "Release time is in the future");
+
+        Grant memory newGrant = Grant({
+            donor: msg.sender,
+            beneficiary: _beneficiary,
+            grantAmount: msg.value,
+            releaseTime: _releaseTime,
+            claimed: false
+        });
+
+        grants[_beneficiary].push(newGrant);
+
+        emit GrantCreated(msg.sender, _beneficiary, msg.value, _releaseTime);
+    }
 }
